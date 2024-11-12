cc8c2fc181bb9cdfa47673686bc2638167b78336
serozhenka <sergeykaoff@gmail.com>
2024-11-12 22:46:55 +0200

    feat: add get current user endpoint

--- server/src/server/routes/users/routes.py
+++ server/src/server/routes/users/routes.py
@@ -1,11 +1,26 @@
 from fastapi import APIRouter
+from sqlalchemy import sql
 
-from server.authentication.utils import protected_route
+from server.authentication.utils import User, protected_route
+from server.db import DbSession
+from server.db.models import User as UserTable
 
 router = APIRouter()
 
 
 @router.get("/me")
 @protected_route
-async def get_me():
-    pass
+async def get_me(db_session: DbSession, user: User):
+    query = (
+        sql.select(
+            UserTable.id,
+            UserTable.username,
+            UserTable.name,
+            UserTable.role,
+            UserTable.created_at,
+        )
+        .where(UserTable.username == user.username)
+        .limit(1)
+    )
+    cursor_result = await db_session.execute(query)
+    return cursor_result.mappings().one()
